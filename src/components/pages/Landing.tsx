190f34ca32c48ec092117127a34b45089b80f82f
Phreakester <gettyv05@gmail.com>
2022-03-07 02:05:01 -0800

    Fixed linting errors

--- src/components/pages/Landing.tsx
+++ src/components/pages/Landing.tsx
@@ -1,13 +1,13 @@
 import { Link } from 'react-router-dom';
 
 function Landing(): JSX.Element {
-  return(
-    <div className="landing_wrapper">
-        <img />
-        <div className="landing_text">
-            Lorem impsum dolor sit amet
-        </div>
-        <Link to="/exploding-gradient" className="link_button">Lets Go!</Link>
+  return (
+    <div className="landing-wrapper">
+      <img />
+      <div className="landing-text">Lorem impsum dolor sit amet</div>
+      <Link to="/exploding-gradient" className="link-button">
+        Lets Go!
+      </Link>
     </div>
   );
 }
