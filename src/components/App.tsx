a714049f67bf41681f5d0762f794ed5d48440e52
Phreakester <gettyv05@gmail.com>
2022-03-07 02:03:13 -0800

    Started styling, added file for landing page.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,27 +1,25 @@
-import {
-  BrowserRouter as Router,
-  Routes, 
-  Route 
-} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import AppWrapper from './shared/AppWrapper';
 import { HeaderSections } from './shared/globalTypes';
 import '../assets/WestwoodSans-Regular.ttf';
 
-import Landing from "./pages/Landing";
+import Landing from './pages/Landing';
 
 function App(): JSX.Element {
   return (
     <div>
       <Router>
         <Routes>
-          <Route path="/" element={
-            <AppWrapper section={HeaderSections.DEFAULT_SECTION}>
-              <Landing />
-            </AppWrapper>
-          } />
+          <Route
+            path="/"
+            element={
+              <AppWrapper section={HeaderSections.DEFAULT_SECTION}>
+                <Landing />
+              </AppWrapper>
+            }
+          />
         </Routes>
-        
       </Router>
     </div>
   );
--- src/components/pages/Landing.tsx
+++ src/components/pages/Landing.tsx
@@ -1,9 +1,15 @@
+import { Link } from 'react-router-dom';
+
 function Landing(): JSX.Element {
-    return(
-        <div>
-            Landing Page
+  return(
+    <div className="landing_wrapper">
+        <img />
+        <div className="landing_text">
+            Lorem impsum dolor sit amet
         </div>
-    );
+        <Link to="/exploding-gradient" className="link_button">Lets Go!</Link>
+    </div>
+  );
 }
 
 export default Landing;
