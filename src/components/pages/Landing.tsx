4c847bc57133309180c6d6392aea0820a7a067ac
Phreakester <gettyv05@gmail.com>
2022-03-08 00:00:31 -0800

    Started styling the landing page with a placeholder LSTM picture and text.

--- src/components/pages/Landing.tsx
+++ src/components/pages/Landing.tsx
@@ -1,11 +1,19 @@
 import { Link } from 'react-router-dom';
 
+import "./../../styles/Landing.scss";
+
+import ai_logo from "./../../assets/ai_logo.svg";
+import lstm_img from "./../../assets/lstm.png";
+
 function Landing(): JSX.Element {
   return (
     <div className="landing-wrapper">
-      <img />
-      <div className="landing-text">Lorem impsum dolor sit amet</div>
-      <Link to="/exploding-gradient" className="link-button">
+      <div className="landing-title" >LSTM Learning Lab</div>
+      <img src={lstm_img} className="landing-lstm" />
+      <div className="landing-text">Long short term memory (LSTM) algorithms are a complex pile of math (and spaghetti).
+      However, they have many extremely useful real-world applications, and are a useful tool for machine learning students.
+      This learning lab provides a more intuitive and accessibile route to learning about these models.</div>
+      <Link to="/exploding-gradient" className="landing-button">
         Lets Go!
       </Link>
     </div>
