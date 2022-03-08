ac619ba1bbbf991b1038bc4ffbf869a35cff5775
Phreakester <gettyv05@gmail.com>
2022-03-08 00:01:43 -0800

    Fixed linting errors

--- src/components/pages/Landing.tsx
+++ src/components/pages/Landing.tsx
@@ -1,9 +1,8 @@
 import { Link } from 'react-router-dom';
 
-import "./../../styles/Landing.scss";
+import './../../styles/Landing.scss';
 
-import ai_logo from "./../../assets/ai_logo.svg";
-import lstm_img from "./../../assets/lstm.png";
+import lstm_img from './../../assets/lstm.png';
 
 function Landing(): JSX.Element {
   return (
@@ -11,7 +10,8 @@ function Landing(): JSX.Element {
       <div className="landing-title" >LSTM Learning Lab</div>
       <img src={lstm_img} className="landing-lstm" />
       <div className="landing-text">Long short term memory (LSTM) algorithms are a complex pile of math (and spaghetti).
-      However, they have many extremely useful real-world applications, and are a useful tool for machine learning students.
+      However, they have many extremely useful real-
+      world applications, and are a useful tool for machine learning students.
       This learning lab provides a more intuitive and accessibile route to learning about these models.</div>
       <Link to="/exploding-gradient" className="landing-button">
         Lets Go!
